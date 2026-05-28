24d5c8bb21ef56019d64f6b02fb43a0bf1b78c1a
sammael-ag <sammael.ag@gmail.com>
2026-05-29 01:20:34 +0200

    PWA pridané technológie pre CrystalCore - Tauri a Rust

---
+++ metro.config.cjs
@@ -0,0 +1,9 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Povieme Metru, aby si nevšímalo zložku src-tauri a jej vnútro
+config.resolver.blacklistRE = /src-tauri\/.*/;
+config.resolver.blockList = /src-tauri\/.*/;
+
+module.exports = config;
