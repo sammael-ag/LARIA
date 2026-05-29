ebd4765f6eaba4b304e89637d90bb913fe7c8256
sammael-ag <sammael.ag@gmail.com>
2026-05-29 21:06:57 +0200

    Vyhodeny stary publicPath z metro configu

--- metro.config.cjs
+++ metro.config.cjs
@@ -6,13 +6,4 @@ const config = getDefaultConfig(__dirname);
 config.resolver.blacklistRE = /src-tauri\/.*/;
 config.resolver.blockList = /src-tauri\/.*/;
 
-// Fix pre GitHub Pages podadresár (/LARIA)
-// Ak beží produkčný export, Metro správne nastaví cestu pre statické súbory
-if (process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS) {
-  config.transformer = {
-    ...config.transformer,
-    publicPath: '/LARIA/_expo/static',
-  };
-}
-
 module.exports = config;
